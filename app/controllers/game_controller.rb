73b4e53a2e1438bb6be46fb3c44aca4a4b90fdd4
Devin Sweeting and Miles Bradt <miles@>
2018-12-12 16:29:24 -0800

    runs user input through start method

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,4 +2,14 @@ class GameController < ApplicationController
   def index
     render :index
   end
+
+  def start
+    start = params.fetch("player_input").upcase
+    binding.pry
+    if start == "START"
+      redirect "/gamestart"
+      render :gamestart
+    end
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/game' => 'game#index'
+  get '/start' => 'game#start'
+  get '/gamestart' => 'game#index'
+
 end

3079370c18423cc3620350e266ecedb70c50c55d
Michael Brown and maly phongsavanh <michael@>
2018-12-12 16:39:55 -0800

    setup spec files, begin seeding.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ActiveRecord::Base
   validates :location_id, :presence => true
   validates :name, :presence => true
   validates :description, :presence => true
+
+  private
+  def method_name
+    
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+location_list = [
+  ["You've just showed up, havn't you?", "this place looks like this first. There's a puzzle active", "This place looks like this later. The puzzle has been solved.", false],
+  ["You've just showed up to this new place, havn't you?", "This is what this other place looked like at first.", "This second place looks like this after the puzzle.", false]
+]
+
+location_list.each do |transition, pre_description, post_description, puzzle_solved|
+  Location.create({
+    :transition => transition,
+    :pre_description => pre_description,
+    :post_description => post_description,
+    :puzzle_solved => puzzle_solved
+  })
+end
+
+locations = Location.all
+
+
+item_list = [
+  [locations[0], 'item 1', "Item 1 does some stuff and things."],
+  [locations[1], 'item 2', "Item 2 does some stuff and things as well."]
+]
+
+item_list.each do |location_id, name, description|
+  Item.create({
+    :location_id => location_id,
+    :name => name,
+    :description => description
+  })

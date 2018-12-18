1d91fbbeca4b35bfa52356ff59d8d63fd25e8ff1
Student <example@example.com>
2018-12-18 12:08:03 -0800

    refactor models to remove global dependancy on player.

--- app/models/game_state.rb
+++ app/models/game_state.rb
@@ -1,59 +1,50 @@
 class GameState
-  attr_accessor :player, :locations, :items, :objectives
+  attr_accessor :player, :player_pre_game, :locations_pre_game, :items_pre_game, :objectives_pre_game
 
-  def initialize(player)
-    @player = player
-    player? = attributes.fetch(:player, player = Player.create({:name => 'chester',:location_id => 1,:objective_id => 0}))
+  def initialize(attributes)
+    @player = attributes.fetch(:player)
+    player = attributes.fetch(:player, player = Player.create({:name => 'chester',:location_id => 0,:objective_id => 0}))
 
-    @objectives = attributes.fetch(:objectives, [@player.objectives.create({:description => 'new objective', :complete => false})])
-
-    if attributes.keys.include?(:locations)
-      @locations = attributes.fetch(:locations)
-    else
-      @objectives[0].locations.create({
-        :transition => 'Welcome!',
-        :pre_description => 'for now, I look like this',
-        :post_description => 'after that thing, I look like this now.',
-        :puzzle_solved => false
-      })
-      @locations = @objectives[0].locations
-
-    end
-
-
-    @items = attributes.fetch(:items,
-      [@locations[0].item = @player.items.create({
-        :name => 'Hammer',
-        :description => 'Shiny metal on a piece of woody stick',
-        :found => false
-      })
-    ])
-
-    if @player.location_id == 0
-      @player.location_id = @locations[0].id
-    end
-
-    # state of game before play.
+    # # state of game before play.
     @player_pre_game = @player
-    @objectives_pre_game = @objectives
-    @locations_pre_game = @locations
-    @items_pre_game = @items
+    @objectives_pre_game = @player.objectives
+    @locations_pre_game = []
+    @items_pre_game = []
+    @objectives_pre_game.each do |objective|
+      objective.locations.each do |location|
+        @locations_pre_game.push(location)
+        @items_pre_game.push(location.item)
+      end
+    end
+    @player
+  end
+
+  def locations
+    locations_array = []
+    @player.objectives.each do |objective|
+      objective.locations.each do |location|
+        locations_array.push(location)
+      end
+    end
+    locations_array
   end
 
   def player_items
     items_held = []
-    @items.each do |item|
-      if item.found
-        items_held.push(item)
+    self.locations.each do |location|
+        item = location.item
+        if item.found
+          items_held.push(item)
+        end
       end
-    end
     items_held
   end
 
   def move_to(location_id)
-    @locations.each do |local|
+    self.locations.each do |local|
       if local.id == location_id
         @player.location_id = location_id
+        @player.update({:location_id => @player.location_id})
         return local.id
       end
     end
--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -2,6 +2,5 @@ require 'rails_helper'
 
 describe Objective do
   it { should have_many :locations }
-  it { should belong_to :player }
 
 end
